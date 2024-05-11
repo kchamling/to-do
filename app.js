959e59b6fbfae02209076e3998c2bce0182f28b0
Kaman Chamling <chamlingkaman@gmail.com>
2024-05-11 17:33:53 +0200

    save in localstorage

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
-const addBtn = document.querySelector(".group button");
-const input = document.querySelector(".group input");
+const addBtn = document.querySelector(".input-add button");
+const input = document.querySelector(".input-add input");
 const toDoList = document.querySelector(".to-do-list");
+
+showData();
 
 addBtn.addEventListener("click", () => {
   if (input.value.length > 0) {
-    const newToDo = document.createElement("li");
-    newToDo.innerHTML = input.value;
-
-    const closIcon = document.createElement("span");
-    closIcon.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-    newToDo.append(closIcon);
+    const newToDo = document.createElement("div");
+    newToDo.classList.add("to-do");
+    newToDo.innerText = input.value;
 
     newToDo.addEventListener("click", () => {
-      newToDo.classList.add("checked");
+      newToDo.classList.toggle("checked");
+      saveData();
     });
 
-    closIcon.addEventListener("click", () => {
+    const closeIcon = document.createElement("span");
+    closeIcon.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    newToDo.append(closeIcon);
+
+    closeIcon.addEventListener("click", () => {
       newToDo.remove();
+      saveData();
     });
 
+    saveData();
     toDoList.append(newToDo);
-    input.value = "";
   }
 });
+
+function saveData() {
+  localStorage.setItem("data", toDoList.innerHTML);
+}
+
+function showData() {
+  toDoList.innerHTML = localStorage.getItem("data");
+  const toDoItems = document.querySelectorAll(".to-do");
+  toDoItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("checked");
+      saveData();
+    });
+    const closeIcon = item.querySelector("span");
+    closeIcon.addEventListener("click", () => {
+      item.remove();
+      saveData();
+    });
+  });
+}
